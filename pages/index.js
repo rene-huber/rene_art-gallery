0083fc2319fb8ebe53206bf9617b45a51bc1f420
rene-huber <96597641+rene-huber@users.noreply.github.com>
2023-12-15 15:52:45 +0100

    spotlight funct and render

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
+import SpotLight from "@/components/SpotLight";
 import Link from "next/link";
+
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+     <h1>Art Gallery</h1>
 <ul>
 <li> 
-  <Link href={"/art-pieces"}>art pieces</Link>
+  <Link href={"/art-pieces"}>art pieces</Link> {""} <br/><br/>
+  <Link href={"/spotlight"}>Spotlight</Link>
+
+  
 </li>
 
 </ul>
--- pages/spotlight/index.js
+++ pages/spotlight/index.js
@@ -1,4 +1,3 @@
-
 import useStore from '@/store';
 import SpotLight from '@/components/SpotLight';
 import randomItem from '@/lib/random-Item';
@@ -6,21 +5,10 @@ import randomItem from '@/lib/random-Item';
 function Spotlight() {
     const { data } = useStore();
 
-    if (!Array.isArray(data.imageSource) || data.imageSource.length === 0) {
-        return null; // O puedes retornar un mensaje de error o lo que prefieras
-      }
+    const randomSelect = randomItem(data);
+    console.log(randomSelect)
 
-
-// console.log(data.imageSource)
-const randomImage = randomItem(data.imageSource)
-
-console.log(randomImage)
-
-
-return (
-    <SpotLight image={randomImage}/>
-  );
-
+    return <SpotLight image={randomSelect.imageSource} />;
 }
 
-export default Spotlight
+export default Spotlight;
